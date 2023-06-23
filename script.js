003c2ffb716d33d3daff8db40aab056e75aa6c75
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-06-23 13:03:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+	// Toggle dark mode
+		function toggleDarkMode() {
+			let toggle = document.getElementById("dark-mode-toggle");
+			let body = document.getElementsByTagName("body")[0];
+			if (toggle.checked) {
+				body.classList.add("dark-mode");
+				localStorage.setItem("dark-mode", "true");
+			} else {
+				body.classList.remove("dark-mode");
+				localStorage.setItem("dark-mode", "false");
+			}
+		}
+
+		// Check for previous dark mode setting
+		function checkDarkMode() {
+			let darkMode = localStorage.getItem("dark-mode");
+			if (darkMode && darkMode === "true") {
+				let toggle = document.getElementById("dark-mode-toggle");
+				toggle.checked = true;
+				toggleDarkMode();
+			}
+		}
+
+		checkDarkMode();
